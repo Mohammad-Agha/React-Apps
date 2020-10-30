7961afad2dddc933ab7a4cff6261f99faf4957b3
Mohammad-Agha <hamadaagha123@gmail.com>
2020-10-30 13:51:44 +0200

    Initial commit

--- redux-example-1/src/App.js
+++ redux-example-1/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import PostForm from './components/PostForm'
-import Posts from './components/Posts'
-import store from './store'
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <Provider store={store}>
-      <div>
-        <PostForm />
-        <hr/>
-        <Posts />
-      </div>
-    </Provider>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
